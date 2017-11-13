9a2b23029792cf89426428af2a81c39bbafe173f
marls628 <marlyshawley@outlook.com>
2017-11-13 01:59:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function setBestCustomer() {
     bestCustomer = 'not bob';
 }
 
-function overwriteBestCustomer () {
+function overwriteBestCustomer() {
     bestCustomer = 'maybe bob'
 }
 
-function changeLeastFavoriteCustomer() {
+changeLeastFavoriteCustomer() {
     leastFavoriteCustomer = 'sour fred';
 }
