0b16e36622ce6fae15da41e5ca712adba70516f0
marls628 <marlyshawley@outlook.com>
2017-11-13 01:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
-var customerName = 'bob'
+var customerName = 'bob';
 
 function upperCaseCustomerName(customerName) {
-    return customerName.toUpperCase('BOB')
+    return customerName = customerName.toUpperCase('BOB');
 }
